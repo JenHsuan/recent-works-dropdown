16987e277f0e190a9158d11a20d155351ab8eb4f
JenHsuan <ofalpha@gmail.com>
2021-10-23 19:23:50 +0800

    add ga

--- src/components/RecentWorksDropDown.tsx
+++ src/components/RecentWorksDropDown.tsx
@@ -5,6 +5,9 @@ const months = 6;
 type Props = {
     title: string;
     gitHubUserName: string;
+    sendGaEvent: (a:any, b:any, c:any, d:any) => {};
+    actionConstant: string;
+    catogoryConstant: string;
 };
 export class RecentWorksDropDown extends Component<Props, any> {
     constructor(props: any) {
@@ -21,11 +24,11 @@ export class RecentWorksDropDown extends Component<Props, any> {
             gitHubUserName
         } = this.props;
 
-            fetch(`https://api.github.com/users/${gitHubUserName}/repos`)
+        fetch(`https://api.github.com/users/${gitHubUserName}/repos`)
                 .then(val => val.json())
                 .then(val => {
                     let sortedRepos = val.filter((ele: any) => {
-                        let timeArray = ele['updated_at'].split('-');
+                        let timeArray = ele.updated_at.split('-');
                         timeArray[2] = timeArray[2].split('T') ? timeArray[2].split('T')[0] : 0;
                         timeArray = timeArray.map((time:any) => Number(time));
                         timeArray[1] = timeArray[1] + months
@@ -37,20 +40,20 @@ export class RecentWorksDropDown extends Component<Props, any> {
                         let today = new Date();
                         return newDate > today;
                     })
-                    .sort((a: any, b: any) => b['updated_at'].localeCompare(a['updated_at']))
+                    .sort((a: any, b: any) => b.updated_at.localeCompare(a.updated_at))
 
-                    console.log(sortedRepos);
                     this.setState((prevState: any) => ({
                         items: sortedRepos
                     }));
-
                 })
-
     }
 
     render() {
         const {
-            title
+            title,
+            sendGaEvent,
+            actionConstant,
+            catogoryConstant
         } = this.props;
         return (
             <div className={style.wrapper}>
@@ -69,6 +72,7 @@ export class RecentWorksDropDown extends Component<Props, any> {
                                     {this.state.items.map((repo:any) => (
                                         <div className={style.dropdownRepoItem}>
                                             <div className={style.dropdownRepoItemTitle} onClick={() => {
+                                                sendGaEvent(catogoryConstant, actionConstant, 1, repo.name);
                                                 window.open(repo.html_url, '_blank');
                                             }}>
                                                 {repo.name}
