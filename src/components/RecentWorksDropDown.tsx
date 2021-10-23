bf05fa5089f3b6f1baaf48cdb5db52f0caca920f
JenHsuan <ofalpha@gmail.com>
2021-10-23 15:55:35 +0800

    improve

--- src/components/RecentWorksDropDown.tsx
+++ src/components/RecentWorksDropDown.tsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
 import style from './RecentWorksDropDown.module.css';
+const months = 6;
 
 type Props = {
     title: string;
+    gitHubUserName: string;
 };
 export class RecentWorksDropDown extends Component<Props, any> {
     constructor(props: any) {
@@ -11,7 +13,41 @@ export class RecentWorksDropDown extends Component<Props, any> {
             showItems: false,
             items: []
         }
+        this.getItems = this.getItems.bind(this);
     }
+
+    getItems() {
+        const {
+            gitHubUserName
+        } = this.props;
+
+            fetch(`https://api.github.com/users/${gitHubUserName}/repos`)
+                .then(val => val.json())
+                .then(val => {
+                    let sortedRepos = val.filter((ele: any) => {
+                        let timeArray = ele['updated_at'].split('-');
+                        timeArray[2] = timeArray[2].split('T') ? timeArray[2].split('T')[0] : 0;
+                        timeArray = timeArray.map((time:any) => Number(time));
+                        timeArray[1] = timeArray[1] + months
+                        if (timeArray[1]  > 12) {
+                            timeArray[0] = timeArray[0] + Math.floor(timeArray[1] / 12);
+                            timeArray[1] = timeArray[1] % 12;
+                        }
+                        let newDate = new Date(timeArray[0], timeArray[1], timeArray[2]);
+                        let today = new Date();
+                        return newDate > today;
+                    })
+                    .sort((a: any, b: any) => b['updated_at'].localeCompare(a['updated_at']))
+
+                    console.log(sortedRepos);
+                    this.setState((prevState: any) => ({
+                        items: sortedRepos
+                    }));
+
+                })
+
+    }
+
     render() {
         const {
             title
@@ -19,7 +55,8 @@ export class RecentWorksDropDown extends Component<Props, any> {
         return (
             <div className={style.wrapper}>
                 <div className={style.dropdownContainer} onClick={() => {
-                    this.setState((prevState:any) => ({ showItems: !prevState.showItems}))
+                    this.setState((prevState: any) => ({ showItems: !prevState.showItems }));
+                    this.getItems();
                 }}>
                     <div className={style.dropdownContainerItem}>{ title }</div>
                 </div>
@@ -28,6 +65,23 @@ export class RecentWorksDropDown extends Component<Props, any> {
                         {this.state.items.length !== 0 ? (
                             <>
                                 <div className={style.dropdownItem}>
+                                    <div className={style.dropdownItemTitle}>{`Last ${months} month`}</div>
+                                    {this.state.items.map((repo:any) => (
+                                        <div className={style.dropdownRepoItem}>
+                                            <div className={style.dropdownRepoItemTitle} onClick={() => {
+                                                window.open(repo.html_url, '_blank');
+                                            }}>
+                                                {repo.name}
+                                            </div>
+                                            <div className={style.dropdownRepoItemDate}>
+                                                {repo.updated_at.split('T')[0]}
+                                            </div>
+                                            <div className={style.dropdownRepoItemFooter}>
+                                                <div className={style.dropdownRepoItemFooterItem}>{`${repo.stargazers_count} stars`}</div>
+                                                <div className={style.dropdownRepoItemFooterItem}>{`${repo.watchers_count} watchers`}</div>
+                                            </div>
+                                        </div>
+                                    ))}
                                 </div>
                             </>) : (
                             <>
